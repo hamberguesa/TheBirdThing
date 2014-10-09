133f1f93381aedad76e5cfb680fc82cb90d76ef0
Amber H. & Jayme <pair+hamberguesa.jayme-mckiney@devbootcamp.com>
2014-10-09 11:59:19 -0700

    changed controller stuff

--- app/controllers/profile_index.rb
+++ app/controllers/profile_index.rb
@@ -3,16 +3,21 @@ get '/' do
   erb :welcome
 end
 
-post '/' do
+post '/signup' do
   # This might be divided into '/log_in' and '/sign_up'
-  redirect '/home_page'
-  end
+  redirect '/home'
+end
+
+post '/login' do
+
+  redirect '/home'
+end
 
 get '/home' do
-  erb :'home_page'
+  erb :home_page
 end
 
 get '/profile/:id' do
-  erb :'/profile'
+  erb :profile
 end
 
--- app/controllers/tweet_index.rb
+++ app/controllers/tweet_index.rb
@@ -1,20 +1,21 @@
 
 
-get '/profile/:id/new_tweet' do
+get '/new_tweet' do
   erb :new_tweet
 end
 
-post '/profile/:id/new_tweet' do
+post '/new_tweet' do
   # Tweets need to know who authors them.
 end
 
-get '/profile/:id/edit_tweet' do
+get '/edit_tweet/:id' do
   erb :edit_tweet
 end
 
-put '/profile/:id/edit_tweet' do
+put '/edit_tweet/:id' do
+
   redirect '/profile/:id'
 end
 
-delete '/profile/delete_tweet' do
+delete '/delete_tweet' do
 end
